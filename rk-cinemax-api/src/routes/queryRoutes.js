92dbefa2d23a8501f6eff9db30b35bf92a96f41d
Sagarika Mohanty <sagarika@Sagarikas-MacBook-Pro.local>
2026-04-20 21:31:36 +0530

    Split frontend into standalone branch

--- rk-cinemax-api/src/routes/queryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitQuery, getQueries } = require('../controllers/queryController');
-const { protect } = require('../middleware/authMiddleware');
-
-router.post('/', submitQuery); // Public
-router.get('/', protect, getQueries); // Admin only
-
-module.exports = router;
